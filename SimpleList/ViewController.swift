ae217c29a763f850064f39e3ff82ac8b12c03830
Isla <Islajiin@gmail.com>
2025-03-30 09:47:29 +0900

    Initial Commit

---
+++ SimpleList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleList
+//
+//  Created by yeonjin on 3/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
